e96c1b0c626b1a58f4d1440034d994ac484318d5
chaehong.lee <lp90891@linecorp.com>
2022-05-27 14:20:53 +0900

    알림리스트

---
+++ src/components/icons/Bell.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="41.835" height="45.248" viewBox="0 0 41.835 45.248">
+        <g id="bell" transform="translate(1 1)">
+            <path id="패스_445" data-name="패스 445" d="M14.938,4.979H27.386q7.469,0,9.959-4.979V39.835q-2.49-4.979-9.959-4.979H14.938A14.938,14.938,0,0,1,0,19.917H0A14.938,14.938,0,0,1,14.938,4.979Z" transform="translate(39.835) rotate(90)" fill="#e9e6ff" stroke="#e9e6ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+            <path id="패스_446" data-name="패스 446" d="M0,14.74A7.37,7.37,0,0,0,7.37,7.37,7.37,7.37,0,0,0,0,0" transform="translate(27.287 36.879) rotate(90)" fill="#e9e6ff"/>
+        </g>
+    </svg>
+</template>
---
+++ src/page/notification/Notification.vue
@@ -0,0 +1,112 @@
+<template>
+    <v-sheet class="mx-auto fill-height w-100" max-width="700">
+        <!-- 리스트 없을경우 -->
+        <div v-if="items.length === 0" class="pos-center text-center pb-15">
+            <Bell/>
+            <div class="fs-14 secondary--text mt-3">도착한 알림이 없습니다.</div>
+        </div>
+        <ul class="list" v-else>
+            <li v-for="(item, index) in items" :key="index" :class="{'new':item.new === true}">
+                <div class="item">
+                    <div class="icon">
+                        <v-img :src="item.icon" contain></v-img>
+                    </div>
+                    <div class="content">
+                        <div class="text" v-text="item.text"></div>
+                        <div class="date" v-text="item.date"></div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </v-sheet>
+</template>
+
+<script>
+import Bell from '@/components/icons/Bell'
+export default {
+    data: ()=> ({
+      selectedItem: 1,
+      items: [
+        { text: '1내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용', 
+          icon: 'https://vuejs.org//images/logo.png', 
+          date: '21.11.01',
+          new: true
+        },
+        { text: '2내용내용내용내용내용내용내용내용내용내용내용내용내용내용', 
+          icon: 'https://vuejs.org//images/logo.png', 
+          date: '21.11.01',
+          new: false
+        },
+        { text: '3내용내용내용내용내용내용내용내용내용내용내용내용내용내용', 
+          icon: 'https://vuejs.org//images/logo.png', 
+          date: '21.11.01',
+          new: false
+        },
+        { text: '4내용내용내용내용내용내용내용내용내용내용내용내용내용내용', 
+          icon: 'https://vuejs.org//images/logo.png', 
+          date: '21.11.01',
+          new: false
+        },
+        { text: '5내용내용내용내용내용내용', 
+          icon: 'https://vuejs.org//images/logo.png', 
+          date: '21.11.01',
+          new: false
+        },
+        { text: '6내용내용내용내용내용내', 
+          icon: 'https://vuejs.org//images/logo.png', 
+          date: '21.11.01',
+          new: false
+        },
+        { text: '7내용내용내용내용내용내용내용', 
+          icon: 'https://vuejs.org//images/logo.png', 
+          date: '21.11.01',
+          new: false
+        },
+      ], 
+    }),
+    components: {
+        Bell
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.list{
+    padding:0;
+    li{
+        padding: 18px 23px 0 23px;
+        .item{
+            display: flex;
+            flex: 1 1 100%;
+            border-bottom: 1px solid #E6E8EA;
+            justify-content: space-between;
+            padding-bottom: 18px;
+            .icon{
+                width: 30px;
+                height: 30px;
+                border-radius: 50%;
+                overflow: hidden;
+                margin-top: 5px;
+            }
+            .content{
+                width: calc(100% - 42px);
+                .date{
+                    font-size: 12px;
+                    color: #545457;
+                    margin-top: 5px;
+                }
+            }
+        }
+        &.new{
+            background: $color-sub;
+        }
+        &:last-child{
+            .item{
+                border-bottom: none;
+            }
+        }
+
+    }
+}
+
+</style>
